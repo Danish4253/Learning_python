b977b299a51ee88328ade81b2c0d6f2a465ba852
Danish4253 <dk41984200@gmail.com>
2026-08-16 15:25:22 +0530

    conditional statement check

--- The-Ultimate-Python-Course-main/The-Ultimate-Python-Course-main/Chapter 6 - PS/01_problem1.py
+++ The-Ultimate-Python-Course-main/The-Ultimate-Python-Course-main/Chapter 6 - PS/01_problem1.py
@@ -3,14 +3,17 @@ a2 = int(input("Enter number 2: "))
 a3 = int(input("Enter number 3: "))
 a4 = int(input("Enter number 4: "))
 
-if(a1>a2 and a1>a3 and a1>a4):
+if a1 < 0 or a2 < 0 or a3 < 0 or a4 < 0:
+    print("Don't use negative numbers")
+
+elif a1 >= a2 and a1 >= a3 and a1 >= a4:
     print("Greatest number is a1:", a1)
 
-elif(a2>a1 and a2>a3 and a2>a4):
+elif a2 >= a1 and a2 >= a3 and a2 >= a4:
     print("Greatest number is a2:", a2)
 
-elif(a3>a1 and a3>a2 and a3>a4):
+elif a3 >= a1 and a3 >= a2 and a3 >= a4:
     print("Greatest number is a3:", a3)
 
-elif(a4>a1 and a4>a2 and a4>a3):
+else:
     print("Greatest number is a4:", a4)
